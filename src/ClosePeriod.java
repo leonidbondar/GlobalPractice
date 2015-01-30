f797123c94b98e0ff3fb459d965e995f7f8676d0
Leonid Bondar <bondarleonid26@gmail.com>
2015-01-30 11:51:43 +0200

    close period fixes

--- src/ClosePeriod.java
+++ src/ClosePeriod.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -13,33 +15,14 @@ public class ClosePeriod extends User {
 		driver.get(baseUrl + "/admin/bonus/bonus");
 
 		// бесконечный цикл, как сделать конечный не придумал
+		// искать будет только на первой странице
 		while (true) {
-			int pageNumber = 1; // это номер страницы с периодами
-
-			// перед началом каждого цикла зануляем object reference чтобы
-			// учесть возможный переход на другую страницу.
-			WebElement element = null;
-			// пока не могут найти кнопку закрыть на первой и т.д. страницах, то
-			// номера страниц должны
-			// увеличиватся
-			/*
-			while (element == null) {
-				
-				try {
-					driver.findElement(By.linkText(String.valueOf(pageNumber)))
-							.click();
-				} catch (Exception e) {
-					try {
-						element = driver.findElement(By.linkText("Закрыть"));
-					} catch (Exception exception) {
-						pageNumber++;
-					}
-				}
+			try {
+				driver.findElement(By.linkText("Закрыть")).click();
+			} catch (NoSuchElementException e) {
+				System.out.println("No period found that could be closed");
+				break;
 			}
-			*/
-			// потом закрывается период, только одна ссылка Закрыть может быть
-			// одновременно
-			driver.findElement(By.linkText("Закрыть")).click();
 			driver.findElement(By.name("saveBonus")).click();
 			driver.findElement(By.id("password")).clear();
 			driver.findElement(By.id("password")).sendKeys("123456");
